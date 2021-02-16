7c5cd120af1c980cc334d11f854544cf27c429d9
Alex Shelest <shelest1982@gmail.com>
2021-02-16 20:30:15 +0200

    Process finished with exit code 0

--- src/main/java/pageobjects/BaseTest.java
+++ src/main/java/pageobjects/BaseTest.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import java.util.concurrent.TimeUnit;
 
 
@@ -12,16 +13,24 @@ public class BaseTest {
 
     @BeforeClass
     public static void setUp() throws Exception{
+        /*
         System.setProperty("webdriver.chrome.driver", "C:\\Chromedriver\\chromedriver.exe");
-        driver = new ChromeDriver();
+        driver = newChromeDriver();
+        */
+
+        System.setProperty("webdriver.gecko.driver", "C:\\geckodriver\\geckodriver.exe");
+        driver = new FirefoxDriver();
+
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://www.sharelane.com/");
+        Thread.sleep(0);
     }
 
     @AfterClass
     public static void tearDown() throws Exception{
         driver.quit();
+        Thread.sleep(0);
     }
 
     public static WebDriver getDriver(){
